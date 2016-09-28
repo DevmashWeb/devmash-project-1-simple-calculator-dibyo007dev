e80c3477193cbb81a2d48f25a7177e93df596d43
dawn_scroll <dibyomails007gmail.com>
2016-09-28 23:51:41 +0530

    new screen

--- script/script.js
+++ script/script.js
@@ -10,14 +10,13 @@ var scr = document.getElementById('screen');
 var input ;
 var output ;
 
-button.addEventListener("click",function(){
-  input = parseInt(inputBox.value);
-  output = Math.pow(input,1/2);
-  outputBox.innerHTML=output;
-});
+var classname = document.getElementsByClassName("butt");
 
+var myFunction = function() {
+    var attribute = this.getAttribute("value");
+    document.getElementById('screen').value=document.getElementById('screen').value + attribute;
+};
 
-button.getElementByClassName('butt').addEventListener("click", myFunc());
-function myFunc() {
-  alert("its done ");
+for (var i = 0; i < classname.length; i++) {
+    classname[i].addEventListener('click', myFunction, false);
 }
